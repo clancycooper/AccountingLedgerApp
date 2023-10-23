024ea12bdbe4a4e292366533e1c28328941c91cf
clancycooper <dudeclancy@gmail.com>
2023-10-23 11:15:45 -0400

    First commit: Created Main, Home Screen, Ledger Screen and Reports Screen

---
+++ src/main/java/com/pluralsight/AccountingLedgerApp.java
@@ -0,0 +1,43 @@
+package com.pluralsight;
+
+/*Welcome to the Main Class for my Accounting Ledger App
+Here is where the Screens of my App will live
+The Home screen will allow users to:
+    D) Add deposit: Prompt user for deposit info and save to csv file
+    P) Make payment: Prompt user for debit info and save to csv file
+    L) Ledger - Display the Ledger Screen
+    X) Exit - Exit app
+The Ledger Screen will allow users to:
+    Show the newest entries first
+    A) All: Display all entries
+    D) Deposits: Display entries that are only deposits
+    P) Payments: Display only the negative entries (payments)
+    R) Reports: New screen for running pre-defined reports/making custom searches
+    H) Return Home
+The Reports Screen will allow users to:
+    1) Month to Date
+    2) Previous Month
+    3) Year to Date
+    4) Previous Year
+    5) Search by Vendor: Prompt user for vendor name and display all entries for that vendor
+    0) Back: Go back to Report page
+    H) Return Home
+ */
+public class AccountingLedgerApp {
+    public static void main(String[] args) {
+
+    }
+
+    public static void homeScreen() {
+
+    }
+
+    public static void ledgerScreen() {
+
+    }
+
+    public static void reportsScreen() {
+
+    }
+}
+
